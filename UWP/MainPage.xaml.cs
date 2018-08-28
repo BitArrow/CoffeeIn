ec9c7c1f5dce3d7a0890dcd4442a3bf362e58e12
BitArrow <tauribusch@hot.ee>
2018-08-29 00:12:39 +0300

    Navigation menu implementation

--- UWP/MainPage.xaml.cs
+++ UWP/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -15,6 +16,7 @@ using Windows.UI.Xaml.Navigation;
 using Microsoft.Extensions.DependencyInjection;
 using UWP.Services;
 using UWP.Services.Interfaces;
+using UWP.Views;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -37,6 +39,34 @@ namespace UWP
         private void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
             _authService = App.Container.GetRequiredService<IAuthService>();
+        }
+
+        private void NavigationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as AppBarButton)?.Name)
+            {
+                case "MenuBtn":
+                    
+                    break;
+                case "SpecialsBtn":
+
+                    break;
+                case "ProfileBtn":
+
+                    break;
+                case "LocationsBtn":
+
+                    break;
+                case "SettingsBtn":
+
+                    break;
+                case "LogoutBtn":
+                    _authService.LogOut();
+                    this.Frame.Navigate(typeof(LoginView));
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
         }
     }
 }
